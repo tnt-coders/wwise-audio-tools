9c4ad1821b006b45842fde131f22dfd2da325563
tjwrona <tjwrona1992@gmail.com>
2026-02-22 16:36:39 -0500

    Explicitly tell conan to not touch the C++ standard during test_package step

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,24 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.build import can_run
 import os
 
 class WwiseAudioToolsTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.blocks.remove("cppstd")
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
